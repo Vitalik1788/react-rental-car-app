61c09f727b5fb8d1700be47046ec805a43e5a31b
Vitalik1788 <tarasenkovitalik2013@ukr.net>
2023-11-05 15:48:09 +0200

    add wrong path redirect

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout/Layout';
 import MainPage from 'pages/MainPage';
 import CatalogPage from 'pages/CatalogPage';
 import FavoritePage from 'pages/FavoritePage';
-import NoPage from './Nopage/Nopage';
-
 
 export const App = () => {
-
-
   return (
     <div>
       <Routes>
@@ -16,7 +12,7 @@ export const App = () => {
           <Route index element={<MainPage />} />
           <Route path="catalog" element={<CatalogPage />} />
           <Route path="favorite" element={<FavoritePage />} />
-          <Route path='*' element={<NoPage />} />
+          <Route path="*" element={<Navigate to={'/'} />} />
         </Route>
       </Routes>
     </div>
